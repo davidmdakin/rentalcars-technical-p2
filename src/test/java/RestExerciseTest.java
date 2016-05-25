7ae448e3207dc48ed532077f6191d59b15408802
davidmdakin <davidmdakin@gmail.com>
2016-05-25 06:07:02 +0100

    UPDATE: Misc gradle files

--- src/test/java/RestExerciseTest.java
+++ src/test/java/RestExerciseTest.java
@@ -5,7 +5,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import main.java.Car;
+import RestExercise.Car;
 
 import static org.junit.Assert.*;
 import java.io.IOException;
@@ -22,11 +22,9 @@ public class RestExerciseTest {
     	try {
 			String jsonString = IOUtils.toString(new URL("http://localhost:8080"), (Charset) null);
     	} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail("MalformedURLException: " + e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			fail("IOException: " + e.getMessage());
 		}
